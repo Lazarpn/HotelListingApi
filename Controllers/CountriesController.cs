7aa5ad4303197c2eb8e7ff8a1f224a5e43310cce
Lazar <lazarst.pn@gmail.com>
2023-03-21 23:34:17 +0100

    Added  Countries Endpoint

---
+++ Controllers/CountriesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelListingApi.Data;
+
+namespace HotelListingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly HotelListingDbContext context;
+
+        public CountriesController(HotelListingDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/Countries
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountries()
+        {
+            var countries = await this.context.Countries.ToListAsync();
+            return Ok(countries); 
+        }
+
+        // GET: api/Countries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Country>> GetCountry(int id)
+        {
+            var country = await this.context.Countries.FindAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return country;
+        }
+
+        // PUT: api/Countries/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry(int id, Country country)
+        {
+            if (id != country.Id)
+            {
+                return BadRequest("Invalid Record Id");
+            }
+
+            this.context.Entry(country).State = EntityState.Modified;
+
+            try
+            {
+                await this.context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Countries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(Country country)
+        {
+            this.context.Countries.Add(country);
+            await this.context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCountry", new { id = country.Id }, country);
+        }
+
+        // DELETE: api/Countries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry(int id)
+        {
+            var country = await this.context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            this.context.Countries.Remove(country);
+            await this.context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CountryExists(int id)
+        {
+            return this.context.Countries.Any(e => e.Id == id);
+        }
+    }
+}
